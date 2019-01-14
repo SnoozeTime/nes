09eb5c3274d49ba6f37e6165e4aed947aff7afde
Benoit Eudier <benoit.eudier@gmail.com>
2019-01-14 11:48:08 +0900

    remove the vectors that were used for hacky perf measurement and were taking all the memory

--- src/nes.rs
+++ src/nes.rs
@@ -63,16 +63,6 @@ impl Nes {
         let mut is_pause = false;
         let mut is_debug = false;
 
-        let mut input_samples: Vec<u32> = Vec::new();
-        let mut cpu_samples = Vec::new();
-        let mut ppu_samples = Vec::new();
-        let mut graph_samples = Vec::new();
-
-        input_samples.reserve(100000);
-        cpu_samples.reserve(100000);
-        ppu_samples.reserve(100000);
-        graph_samples.reserve(100000);
-
         // Fixed time stamp for input polling.
         let fixed_time_stamp = Duration::new(0, 16666667); 
         let mut previous_clock = Instant::now();
@@ -81,18 +71,12 @@ impl Nes {
         'should_run: loop {
             // Update CPU and PPU (and later APU)
             // if !is_pause {
-            let now_cpu = Instant::now();
             let cpu_cycles = self.cpu.next(&mut self.memory)?;
-            cpu_samples.push(now_cpu.elapsed().subsec_micros());
-
-            let now_ppu =Instant::now();
             self.ppu.next(3*cpu_cycles, &mut self.memory, is_debug)?;
-            ppu_samples.push(now_ppu.elapsed().subsec_micros());
-            //}
+            
             // handle events.
             while accumulator > fixed_time_stamp {
                 accumulator -= fixed_time_stamp;
-                let now_input = Instant::now();
                 match self.ui.handle_events(&mut self.memory, is_pause) {
                     Some(EmulatorInput::QUIT) => break 'should_run,
                     Some(EmulatorInput::PAUSE) => is_pause = !is_pause,
@@ -105,11 +89,8 @@ impl Nes {
                     },
                     None => {},
                 }
-                input_samples.push(now_input.elapsed().subsec_micros());
                 // render
-                let now_graph = Instant::now();
                 self.ui.display(&mut self.memory, &mut self.ppu);
-                graph_samples.push(now_graph.elapsed().subsec_micros());
             }
 
             accumulator += Instant::now() - previous_clock;
@@ -124,34 +105,6 @@ impl Nes {
             // }
         }
 
-        let mut avg_input = 0;
-        for sample in &input_samples {
-            avg_input += *sample;
-        }
-        //avg_input /= input_samples.len() as u32;
-
-        let mut avg_cpu = 0;
-        for sample in &cpu_samples {
-            avg_cpu += *sample;
-        }
-        //avg_cpu /= cpu_samples.len() as u32;
-
-        let mut avg_ppu = 0;
-        for sample in &ppu_samples {
-            avg_ppu += *sample;
-        }
-        //avg_ppu /= ppu_samples.len() as u32;
-
-        let mut avg_graph = 0;
-        for sample in &graph_samples {
-            avg_graph += *sample;
-        }
-        //avg_graph /= graph_samples.len() as u32;
-
-        println!("INPUT: {}", avg_input);
-        println!("CPU: {}", avg_cpu);
-        println!("PPU: {}", avg_ppu);
-        println!("GRAPH: {}", avg_graph);
         Ok(())
     }
 
